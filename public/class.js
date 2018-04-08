092fd514d8d282c4d3d88992c0af4b1365f5421a
Hayk09 <hayk.hayrapetyan09@gmailcom>
2018-04-08 13:22:45 +0400

    class.js

--- public/class.js
+++ public/class.js
@@ -1,11 +1,8 @@
-class Grass  {
-    constructor(x, y, index) {
+Class Creature{
+    constructor(x,y,index){
         this.x = x;
         this.y = y;
         this.index = index;
-        this.multiply = Math.round(Math.random() * 8);
-        this.speed = 8;
-        matrix[this.y][this.x] = this.index;
         this.directions = [
             [this.x - 1, this.y - 1],
             [this.x, this.y - 1],
@@ -17,7 +14,6 @@ class Grass  {
             [this.x + 1, this.y + 1]
         ];
 
-    }
     yntrelVandak(ch) {
         var found = [];
         for (var i in this.directions) {
@@ -32,16 +28,6 @@ class Grass  {
         return found;
     }
 
-    mul() {
-        this.multiply++;
-        this.direction = random(this.yntrelVandak(0));
-        if (this.multiply >= this.speed && this.direction) {
-            var newGrass = new Grass(this.direction[0], this.direction[1], this.index);
-            newGrass.parentX = this.x;
-            newGrass.parentY = this.y;
-            grassArr.push(newGrass);
-            matrix[this.direction[1]][this.direction[0]] = this.index;
-            this.multiply = 0;
-        }
+
     }
 }
